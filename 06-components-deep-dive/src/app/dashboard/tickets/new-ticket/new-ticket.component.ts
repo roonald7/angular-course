7766e02e1c5f84bad1e5dab98edf6ca23e1a979d
roonald7 <roonald.anderson017@gmail.com>
2025-10-06 15:42:16 -0300

    Unit 06: Working with templates

--- 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-components-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
 
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+
+  onSubmit(ticketTitle: string, ticketDescription: string) {
+    console.log('Ticket Submitted\nTitle: ', ticketTitle, '\nDescription: ', ticketDescription);
+
+    this.form().nativeElement.reset();
+  }
 }
