a8b897d53c9397bda37544311545933353c0db0d
roonald7 <roonald.anderson017@gmail.com>
2025-10-11 11:38:27 -0300

    Unit 14 - Started Routing module

---
+++ 14-routing/src/app/app.routes.ts
@@ -0,0 +1,27 @@
+import { Routes } from "@angular/router";
+
+import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: NoTaskComponent
+    },
+    {
+        path: 'users/:userId',
+        component: UserTasksComponent,
+        children: [
+            {
+                path: 'tasks',
+                component: TasksComponent
+            },
+            {
+                path: 'tasks/new',
+                component: NewTaskComponent
+            }
+        ]
+    }
+]
